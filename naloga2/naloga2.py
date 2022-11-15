def960e82290f883c4ff26f1c89a9e237c7e9a23
tjazerzen <erzen.tjaz@gmail.com>
2022-11-15 14:18:06 +0100

    Koncana druga naloga.

--- naloga2/naloga2.py
+++ naloga2/naloga2.py
@@ -1,18 +1,27 @@
 # https://putka-upm.acm.si/tasks/t/podkupnine/
 
 import sys
+from functools import lru_cache
 
-lines = sys.stdin.readlines()
+stevilo_primerov = int(input())
 
-stevilo_primerov = int(lines.pop(0).strip())  # map(int, lines.pop(0).strip().split(" "))
-print(stevilo_primerov)
+@lru_cache(None)
+def vsota_geom_zaporedja_rec(potenca, q, t):
+    if potenca == 0:
+        return 1
+    if potenca % 2 == 0:
+        return (vsota_geom_zaporedja_rec(potenca//2, q, t) + pow(q, potenca // 2 + 1, t) * vsota_geom_zaporedja_rec(potenca // 2 - 1, q, t)) % t
+    else:
+        return ((1 + pow(q, (potenca + 1) // 2, t)) * vsota_geom_zaporedja_rec(potenca // 2, q, t)) % t
+
 
 for _ in range(stevilo_primerov):
-    lines.pop(0)  # Na začetku bomo imeli vedno prazno vrstico
-    n, m, q, t = map(int, lines.pop(0).strip().split(" "))
-    print(n, m, q, t)
+    input()
+    n, m, q, t = map(int, input().strip().split(" "))
+    vrednost = 0
     for _ in range(m):
-        a_i, b_i, c_i = map(int, lines.pop(0).strip().split(" "))
-        print(a_i, b_i, c_i)
-
-# n, k, l = map(int, lines.pop(0).strip().split(" "))
+        a_i, b_i, c_i = map(int, input().strip().split(" "))
+        # vrednost += c_i * pow(q, a_i-1, t) * (((1-q**(b_i-a_i+1)) // (1-q)) % t)
+        vrednost += c_i * pow(q, a_i - 1, t) * vsota_geom_zaporedja_rec(b_i - a_i, q, t)
+        vrednost = vrednost % t
+    print(vrednost)
