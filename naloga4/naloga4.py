edda8726653be05a38f6f59dc67ab61dd91988a3
tjazerzen <erzen.tjaz@gmail.com>
2023-01-14 11:51:24 +0100

    Posodobil nalogo 4 za oddajo.

--- naloga4/naloga4.py
+++ naloga4/naloga4.py
@@ -1,49 +1,40 @@
-# https://putka-upm.acm.si/tasks/t/ceste/sl/
+# Naloga 4: Ceste
+# Tjaž Eržen, 27201009
+# finančna matematika, 3. letnik
 
-"""
-Testni primeri:
-test1.txt: Že napisan v nalogi
-test2.txt: Pot s prvim najnižjim avtom ne bo dobra, ker je predraga
-test3.txt: Pot s prvim najnižjim avtom ne bo dobra, ker je predolga
-test4.txt. Nobena rešitev ne dobra (pričakujem -1)
-"""
+# https://putka-upm.acm.si/tasks/t/ceste/sl/
 
 import heapq
 
 ZACETNE_MAX_UTEZI = 10001
+h_max = 10**6+1
 stevilo_primerov = int(input())
 
 
-def rekonstrukcija_poti(seznam_predhodnikov, s, t):
-    lst = [t]
-    idx = t
-    while seznam_predhodnikov[idx] is not None:
-        lst.append(seznam_predhodnikov[idx])
-        idx = seznam_predhodnikov[idx]
-    return lst[::-1]
+def get_result_from_visited_dict(visited, t):
+    visited_final_point_list = [tupple for tupple in visited.keys() if tupple[-1] == t]
+    if not visited_final_point_list:
+        return -1, -1, 1
+    h, d, c, u = sorted(visited_final_point_list)[0]
+    return h, d, c
 
 
-def mod_dijkstra(G, s, t, n):
+def mod_dijkstra(G, s, t, C, D):
     """Vrne minimalno visino avta, ki jo potrebujemo, da pridemo od vozlisca s do t na grafu G."""
-    visited = [False] * (n + 1)
-    cene_najkrajsih_poti = [False] * (n + 1)
-    dolzine_najkrajsih_poti = [False] * (n + 1)
-    max_utezi = [ZACETNE_MAX_UTEZI] * (n + 1)
-    predhodnik = [None] * (n + 1)
-    pq = [(0, s)]  # Prvi argument predstavlja najmanjšo višino avta, da prečkamo reko
+    visited = {}
+    # Priority queue bo iz tupplov dolzine 4 oblike (h_i, d_i, c_i, u)
+    pq = [(0, 0, 0, s)]  # Prvi argument predstavlja najmanjšo višino avta, da prečkamo reko
     while pq:
-        h, u = heapq.heappop(pq)
-        if visited[u]:
+        h, d, c, u = heapq.heappop(pq)
+        if visited.get((h, d, c, u), False):
             continue
-        visited[u] = True
-        max_utezi[u] = h  # To bo pravilno nastavljeno, ker je to min kopica
+        visited[(h, d, c, u)] = True
         for v_i, c_i, d_i, h_i in G[u]:
-            if not visited[v_i]:
-                heapq.heappush(pq, (max(h, h_i), v_i))
-                cene_najkrajsih_poti[v_i] = c_i + cene_najkrajsih_poti[u]
-                dolzine_najkrajsih_poti[v_i] = d_i + dolzine_najkrajsih_poti[u]
-                predhodnik[v_i] = u
-    return max_utezi[t], dolzine_najkrajsih_poti[t], cene_najkrajsih_poti[t], rekonstrukcija_poti(predhodnik, s, t)
+            if visited.get(v_i, False):
+                continue
+            if c + c_i <= C and d + d_i <= D:
+                heapq.heappush(pq, (max(h, h_i), d+d_i, c+c_i, v_i))
+    return visited
 
 
 for _ in range(stevilo_primerov):
@@ -54,5 +45,9 @@ for _ in range(stevilo_primerov):
     for _ in range(m):
         u_i, v_i, c_i, d_i, h_i = map(int, input().strip().split(" "))
         seznam_sosednosti[u_i].append((v_i, c_i, d_i, h_i))
-    minimalna_visina = mod_dijkstra(seznam_sosednosti, s, t, n)
-    print(minimalna_visina)
+    visited_dict = mod_dijkstra(seznam_sosednosti, s, t, C, D)
+    h, d, c = get_result_from_visited_dict(visited_dict, t)
+    if h == -1:
+        print(h)
+    else:
+        print(f"{h} {d} {c}")
